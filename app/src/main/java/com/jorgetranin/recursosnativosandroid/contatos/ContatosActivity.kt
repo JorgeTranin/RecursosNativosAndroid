1659918e700423864cc0b17c9f434ca7a0522906
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2023-04-28 20:10:37 -0400

    Finalização da activity de interação com os contatos do usuarios

--- app/src/main/java/com/jorgetranin/recursosnativosandroid/contatos/ContatosActivity.kt
+++ app/src/main/java/com/jorgetranin/recursosnativosandroid/contatos/ContatosActivity.kt
@@ -1,17 +1,18 @@
 package com.jorgetranin.recursosnativosandroid.contatos
 
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.view.LayoutInflater
 import androidx.core.app.ActivityCompat
-import androidx.recyclerview.widget.RecyclerView
-import com.jorgetranin.recursosnativosandroid.Manifest
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.jorgetranin.recursosnativosandroid.databinding.ActivityContatosBinding
 
 class ContatosActivity : AppCompatActivity() {
     private val REQUEST_CONTACT = 1
+    val LINEAR_LAYOUT_VERTICAL = 1
     lateinit var binding: ActivityContatosBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,13 +24,9 @@ class ContatosActivity : AppCompatActivity() {
                 PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_CONTACTS),
             REQUEST_CONTACT)
-        }else{
-
         }
 
 
-        val recyclerView: RecyclerView = binding.rvContatos
-        recyclerView.adapter = recyclerView.adapter
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -37,6 +34,7 @@ class ContatosActivity : AppCompatActivity() {
         if (requestCode == REQUEST_CONTACT) setContacts()
     }
 
+    @SuppressLint("Range")
     private fun setContacts() {
 
         val contatoList: ArrayList<Contato> = ArrayList()
@@ -44,9 +42,18 @@ class ContatosActivity : AppCompatActivity() {
             null,null,null,null)
         if (cursor != null){
             while (cursor.moveToNext()){
-                contatoList.add(Contato(cursor.getString(cursor.getColumnIndex
-                    (ContactsContract.CommonDataKinds.Phone))))
+                contatoList.add(Contato(
+                    cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)),
+                    cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                ))
             }
+            cursor.close()
         }
+        val adapter = AdapterContatos(contatoList)
+        val contatoRecyclerView = binding.rvContatos
+
+        contatoRecyclerView.layoutManager = LinearLayoutManager(this,
+            LINEAR_LAYOUT_VERTICAL, false)
+        contatoRecyclerView.adapter = adapter
     }
 }
